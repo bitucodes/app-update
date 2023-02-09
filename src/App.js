bea11879e059d95eee78a24665d72534e8b97e75
Bitupan Patowary <86779337+bitucodes@users.noreply.github.com>
2023-02-09 23:32:59 +0530

    basic app

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { ChatEngine } from 'react-chat-engine';
+
+
+const App = () => {
+  return (
+      <ChatEngine
+        height="100vh"
+        projectID="cb9d20f0-6b63-4bf6-9425-42adb45b239f"
+        userName="testingapp"
+        userSecret="12345"
+      />
+  )
+}
+
+export default App
